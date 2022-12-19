bbf4aea69b71fa044f2c797b5c9ae82f09d6b084
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-20 01:53:58 +0530

    Map Function

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import { useState } from "react";
-
 function App() {
-  let str1 = "Home";
-  let str2 = "Explore";
-  let str3 = "Notifications";
-  //let counter = 1;
-  let [counter, setCounter] = useState(1);
-
-  //let city="mumbai";
-  let [city, setCity] = useState("mumbai");
-  let changeCity = () => {
-    city = "Hello " + city;
-
-    //RE-RENDERING THE CITY VALUE
-    setCity(city);
-  };
-
-  let increment = () => {
-    //logical part
-    counter++;
-    console.log(counter);
-
-    //DOM Update Part
-    setCounter(counter);
-  };
+  let title = "Map Demo";
+  let user = { id: 1, city: "mumbai" };
 
   return (
     <div>
-      <h1>{city}</h1>
-      <input type="button" value="Change City" onClick={changeCity} />
-      <hr />
-      <h1>{str1}</h1>
-      <h1>{str2}</h1>
-      <h1>{str3}</h1>
-      <h1>{counter}</h1>
-      <input type="button" value="Increment" onClick={increment} />
+      <h1> {title} </h1>
+      <h1>{user.city}</h1>
     </div>
   );
 }
+
 export default App;
