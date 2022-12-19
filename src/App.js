e605d7ad4faaabb798add9b41ffcbc4243192e3f
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-20 01:36:16 +0530

    stateless variable

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 // USER DEFINED TAGS == COMPONENT
 function App() {
-  let project = "Counter Application";
-  let btnValue = "Click Meeeeeee";
+  //stateless variable
+  let counter = 100;
 
-  //Member :: Type is Function
-  let clickMe = () => alert();
-  let increment = () => {};
-  let decrement = () => {};
+  let increment = () => {
+    counter = counter + 1;
+    console.log(counter);
+  };
 
   //JSX => HTML CSS
   return (
     <div>
-      <h1>{project}</h1>
-      <h1></h1>
-      <input type="button" value={btnValue} onClick={clickMe} />
+      <h1>Counter Application</h1>
+
+      <h1>{counter}</h1>
+
       <input type="button" value="Increment" onClick={increment} />
     </div>
   );
