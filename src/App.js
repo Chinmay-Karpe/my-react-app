dd4365cd8d6bbcdee65e70f8e1050900bea724bd
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-19 21:38:55 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+// JSX!
+// JSX => JAVA SCRIPT + XML
+// XML => Extended the HTML and Cratedad new Tag
+// App => <App />
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
 
-export default App;
+/*function normalFn() {
+  // ..
+  // ..
+  // ..
+  return 100;
+  return "abcd"
+  return true;
+  return [];
+  return {};
+}*/
