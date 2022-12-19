2f27acdf1147745074b34d10a38905a8d4777704
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-20 01:40:19 +0530

    statefull variable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-// USER DEFINED TAGS == COMPONENT
+import { useState } from "react";
+
 function App() {
-  //stateless variable
-  let counter = 100;
+  //let counter = 10;
+  let [counter, setCounter] = useState(10);
 
   let increment = () => {
     counter = counter + 1;
-    console.log(counter);
+
+    //RE-RENDER
+    setCounter(counter);
   };
 
-  //JSX => HTML CSS
   return (
     <div>
       <h1>Counter Application</h1>
