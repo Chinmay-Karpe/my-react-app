78da97b0812d8408f9bd844e22823a775ef829a0
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-20 02:03:48 +0530

    Map Demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 function App() {
   let title = "Map Demo";
-  let list = ["mumbai", "kochi"];
+  let [list, setList] = useState([]);
+
+  let addItem = () => {
+    // logical part
+    let newList = [...list, "NAVI MUMBAI"];
+
+    // dom part
+    setList(newList);
+  };
 
   return (
     <div>
       <h1> {title} </h1>
+      <input type="button" value="Add Item Into List" onClick={addItem} />
 
       {list.map((item) => (
         <h1>{item}</h1>
@@ -14,4 +25,3 @@ function App() {
 }
 
 export default App;
-// [delhi, mumbai] => [<h1>delhi</h1>, <h1>mumbai</h1>]
