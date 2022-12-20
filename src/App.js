7248441c1eaedff7a4684a30cc53341e6189b910
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-20 21:27:01 +0530

    types of parameter

--- src/App.js
+++ src/App.js
@@ -1,60 +1,47 @@
-import { useState } from "react";
-
 function App() {
-  let list = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
-  let [theme, setTheme] = useState("primary");
-
-  //Member Function
-  let makePrimaryTheme = () => {
-    theme = "primary";
-    setTheme(theme);
+  /** Default No param */
+  let clickMe = () => {
+    console.log("Hello");
   };
 
-  let makeSuccessTheme = () => {
-    theme = "success";
-    setTheme(theme);
+  /** Default Event Param */
+  let clickMe2 = (e) => {
+    console.log(e);
   };
 
-  let makeDangerTheme = () => {
-    theme = "danger";
-    setTheme(theme);
+  /** Custom Param */
+  let clickMe3 = (p1) => {
+    console.log(p1);
+  };
+
+  /** Event + Custom Param */
+  let clickMe4 = (e, p1) => {
+    console.log(e, p1);
   };
 
   return (
     <div>
-      <div className="sticky-top">
-        <h1 className={`bg-${theme} text-light p-3`}>
-          Bootstrap Styling!! {theme}
-        </h1>
+      <h1>Btn Click Demo</h1>
 
-        <input
-          className="btn btn-primary"
-          type="button"
-          value="Primary"
-          onClick={makePrimaryTheme}
-        />
-        <input
-          className="btn btn-success"
-          type="button"
-          value="Success"
-          onClick={makeSuccessTheme}
-        />
-        <input
-          className="btn btn-danger"
-          type="button"
-          value="Danger"
-          onClick={makeDangerTheme}
-        />
-      </div>
+      {/** Default No param */}
+      <input type="button" value="clickMe" onClick={clickMe} />
 
-      {list.map((item) => (
-        <div className={`alert alert-${theme} my-1`}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia pariatur
-          non temporibus? Modi magnam velit ex, optio doloremque rerum sit
-          eveniet animi eaque beatae officiis consequatur voluptate accusantium
-          ipsa corporis?
-        </div>
-      ))}
+      {/** Default Event Param */}
+      <input type="button" value="clickMe2" onClick={clickMe2} />
+
+      {/** Custom Param */}
+      <input
+        type="button"
+        value="clickMe3"
+        onClick={() => clickMe3("primary")}
+      />
+
+      {/** Event + Custom Param */}
+      <input
+        type="button"
+        value="clickMe4"
+        onClick={(e) => clickMe4(e, "adsf afd")}
+      />
     </div>
   );
 }
