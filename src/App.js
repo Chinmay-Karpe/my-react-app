e963e3b9ee77371f192e0377b8bdf0096c5218cc
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-21 16:51:19 +0530

    make api get call

--- src/App.js
+++ src/App.js
@@ -1,64 +1,36 @@
+import axios from "axios";
 import { useState } from "react";
 
 function App() {
-  let [message, setMessage] = useState("Hello");
-  let [list, setList] = useState([
-    { message: "Hi", messageTime: new Date() },
-    { message: "How r u", messageTime: new Date() },
-    { message: "Good", messageTime: new Date() },
-    { message: "fine bro", messageTime: new Date() },
-  ]);
+  let [title] = useState("API DEMO");
+  let [messageList, setMessageList] = useState([]);
 
-  // Member fn
-  let updateInputMessage = (e) => {
-    message = e.target.value;
-    setMessage(message);
-  };
+  //Functions
+  let getAllMessages = async () => {
+    let url = `http://localhost:3001/messages`;
+    let response = await axios.get(url);
+    //console.log(response);
 
-  let addMessage = () => {
-    //let newMessage = { message: "Chill bro...!", messageTime: new Date() };
-    let newMessage = { message: message, messageTime: new Date() };
-    list = [newMessage, ...list];
-    setList(list);
-    setMessage("");
-  };
-
-  let checkEnterEvent = (e) => {
-    if (e.keyCode === 13) {
-      addMessage();
-    }
+    //Getting the Message from Server :: And re-rendering
+    messageList = [...response.data];
+    setMessageList(messageList);
   };
 
   return (
     <div>
-      <h1 className="bg-primary text-white p-3">Map Demo</h1>
-      <div className="d-flex">
-        <input
-          className="form-control"
-          value={message}
-          onChange={updateInputMessage}
-          type="text"
-          placeholder="Enter Message"
-          onKeyUp={checkEnterEvent}
-        />
-        <input
-          className="btn btn-primary"
-          type="button"
-          value="Add Message"
-          onClick={addMessage}
-        />
-      </div>
-      {list.map((item, index) => (
-        <div key={index} className="d-flex my-1">
-          <div className="badge text-bg-primary">
-            {item.message}
-            <span className="ms-4">
-              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
-            </span>
-          </div>
-        </div>
+      <h1>{title}</h1>
+
+      <input
+        type="button"
+        value="Make Ajax/API Call"
+        onClick={getAllMessages}
+      />
+
+      {messageList.map((item) => (
+        <div>{item.message}</div>
       ))}
     </div>
   );
 }
+
 export default App;
