f478c1786a972c46b8b9ff57c6284ac2d8df13db
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-23 19:03:34 +0530

    code splitting , multiple component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
   return (
-    <Routes>
-      {/** http://localhost:3000/ */}
-      <Route path="/" element={<h1>Home</h1>} />
+    <div>
+      <AppHeader />
+      <AppBody />
+      <AppBody />
+      <AppFooter />
+    </div>
+  );
+}
 
-      {/** http://localhost:3000/home */}
-      <Route path="/home" element={<h1>Home</h1>} />
+// <AppHeader />
+function AppHeader() {
+  return (
+    <div className="bg-dark text-light p-3">
+      <div className="fs-3">Shopping Book</div>
+    </div>
+  );
+}
 
-      {/** http://localhost:3000/explore */}
-      <Route path="/explore" element={<h1>Explore</h1>} />
+function AppBody() {
+  let [list] = useState([{}, {}, {}, {}, {}, {}, {}, {}]);
 
-      {/** http://localhost:3000/notifications */}
-      <Route path="/notification" element={<h1>Notifications</h1>} />
+  return (
+    <div className="row">
+      {list.map((item, index) => (
+        <div key={index} className="col-12 col-md-3 my-2">
+          <div className="card">
+            <img
+              src={`https://picsum.photos/${250 + index}`}
+              alt=""
+              style={{ height: "200px", objectFit: "cover" }}
+            />
+            <div className="card-header">Card Title</div>
+            <div className="card-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              maxime quam atque vero molestias! Nulla, earum voluptatem culpa id
+              quis deserunt eveniet modi. Inventore excepturi eaque ex mollitia
+              deserunt exercitationem!
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
-      <Route path="*" element={<h1>Page Not Found</h1>} />
-    </Routes>
+function AppFooter() {
+  return (
+    <div
+      className="bg-secondary d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "400px" }}
+    >
+      <div className="text-light fs-4">Copy Right by Student Community!</div>
+      <div className="text-light fs-6">Follow Us @Twitter</div>
+      <div className="text-light fs-6"> Follow Us @Youtube</div>
+    </div>
   );
 }
 
