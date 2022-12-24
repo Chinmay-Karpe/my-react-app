1c2613d46a0f68d2339dc8c9a0d30bdbc458ba11
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-24 17:50:22 +0530

    state management across the component is issue

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import { useState } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <ErrorBoundary FallbackComponent={HandleError}>
-      <HelloWorld />
-    </ErrorBoundary>
-  );
-}
-
-function HandleError({ error }) {
-  console.log("This eror occoured", error);
-
-  return (
     <div>
-      <h1>An Error Occured</h1>
+      <Counter1 />
+      <hr />
+      <Counter2 />
     </div>
   );
 }
 
-function HelloWorld() {
-  let [user] = useState({ id: 1, name: "rohit" });
+function Counter1() {
+  let [amount, setAmount] = useState(100);
+
+  let deposit = () => {
+    amount += 50;
+    setAmount(amount);
+  };
+
   return (
     <div>
-      <h1>Hello World</h1>
-      <h1>{user}</h1>
+      <h1>Counter 1 - Amount {amount}</h1>
+      <input type="button" value="Deposit" onClick={deposit} />
+    </div>
+  );
+}
+
+function Counter2() {
+  let [amount, setAmount] = useState(100);
+
+  let deposit = () => {
+    amount += 50;
+    setAmount(amount);
+  };
+
+  return (
+    <div>
+      <h1>Counter 2 - Amount {amount}</h1>
+      <input type="button" value="Deposit" onClick={deposit} />
     </div>
   );
 }
