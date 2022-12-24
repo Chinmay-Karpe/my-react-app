8d51e3fec96a24e750ca4b48135ee47d30cfa840
karpechinmay29@gmail.com <karpechinmay29@gmail.com>
2022-12-24 17:17:46 +0530

    validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,87 @@
-import AppBody from "./components/AppBody";
-import AppFooter from "./components/AppFooter";
-import AppHeader from "./components/AppHeader";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  // Data Member
+  let inputRef = useRef();
+  let [title] = useState("API DEMO");
+  let [message, setMessage] = useState("");
+  let [messageList, setMessageList] = useState([]);
+
+  // Spl Funcn :: Hook :: Like Constructor ::  Called while the Compoent is Initialized.
+  useEffect(() => {
+    // console.log("I AM GETTING CALLED");
+    getAllMessages();
+  }, []);
+
+  // Member Funcns
+  let handleOnChangeMessage = (e) => {
+    message = e.target.value;
+    setMessage(message);
+    // setMessage(e.target.value)
+  };
+
+  let getAllMessages = async () => {
+    let url = `http://localhost:3001/messages`;
+    let response = await axios.get(url);
+    // console.log(response);
+
+    // Getting the Message From Server :: And re-rendering
+    messageList = [...response.data];
+    setMessageList(messageList);
+  };
+
+  let createNewMessage = async () => {
+    let url = `http://localhost:3001/message`;
+    // console.log(inputRef.current);
+    if (!inputRef.current.checkValidity()) {
+      alert("Invalid");
+      return;
+    }
+
+    let data = {
+      message: message,
+      messageTime: new Date(),
+      reply: true,
+    };
+
+    await axios.post(url, data);
+
+    setMessage("");
+
+    // To Refresh the content
+    getAllMessages();
+  };
+
+  let checkEnterCode = (e) => {
+    if (e.keyCode === 13) {
+      createNewMessage();
+    }
+  };
+
   return (
     <div>
-      <AppHeader />
-      <AppBody />
-      <AppBody />
-      <AppFooter />
+      <h1>{title}</h1>
+
+      <input
+        type="text"
+        placeholder="Hi...whatsapp...!!"
+        value={message}
+        onChange={handleOnChangeMessage}
+        onKeyUp={checkEnterCode}
+        ref={inputRef} // document.querySelector()
+        required
+        minLength={2}
+      />
+      <input
+        type="button"
+        value="Make Ajax/API POST Call"
+        onClick={createNewMessage}
+      />
+
+      {messageList.map((item, index) => (
+        <div key={index}>{item.message}</div>
+      ))}
     </div>
   );
 }
